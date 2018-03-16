624506b90490bbe580e010a0a6f8f85456e0d90a
sdvg789 <sdvg789@naver.com>
2018-03-16 17:51:20 +0900

    03-16
    마무으리

--- src/main/java/kr/ac/jejunu/UserDao.java
+++ src/main/java/kr/ac/jejunu/UserDao.java
@@ -5,10 +5,7 @@ import java.sql.*;
 public class UserDao {
     public User get(int id) throws ClassNotFoundException, SQLException {
         //mysql driver load
-        Class.forName("com.mysql.jdbc.Driver");
-        //Connection 맺고
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/jeju?useSSL=false"
-                , "root", "asdqwe12!");
+        Connection connection = getConnection();
         //sql 작성하고
         PreparedStatement preparedStatement =
                 connection.prepareStatement("select * from userinfo where id = ?");
@@ -27,5 +24,41 @@ public class UserDao {
         connection.close();
         //결과를 리턴한다.
         return user;
+    }
+
+
+    public Integer insert(User user) throws ClassNotFoundException, SQLException {
+        //mysql driver load
+        Connection connection = getConnection();
+        //sql 작성하고
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("insert into userinfo(name, password) values (?, ?)");
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+        //sql 실행하고
+        preparedStatement.executeUpdate();
+        //결과를 User 에 매핑하고
+
+        preparedStatement = connection.prepareStatement("select last_insert_id()");
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+
+        Integer id = resultSet.getInt(1);
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+        //자원을 해지하고
+        //결과를 리턴한다.
+
+        return id;
+    }
+
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        //Connection 맺고
+        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?useSSL=false&characterEncoding=utf-8"
+                , "root", "asdqwe12!");
     }
 }
